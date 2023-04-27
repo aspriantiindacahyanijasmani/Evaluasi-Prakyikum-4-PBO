ac6d494a392a619250042a2369f279caf28032f4
aspriantiindacahyanijasmani <127595682+aspriantiindacahyanijasmani@users.noreply.github.com>
2023-04-27 04:58:05 -1200

    Add files via upload

---
+++ nomor 1/user.java
@@ -0,0 +1,25 @@
+package com.mycompany.aku;
+
+
+class user {
+    private String username;
+    private String password;
+    
+    //ini methid setter
+    public void setUsername(String username){
+        this.username = username;
+    }
+    
+    public void setPassword(String password){
+        this.password = password;
+    }
+    
+    //ini method getter
+    public String getUsername(){
+        return this.username;
+    }
+    
+    public String getPassword(){
+        return this.password;
+    }
+}
